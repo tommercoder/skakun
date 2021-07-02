35363a5143db08592621419aa5b464c0d7daa4ad
Golem <segeda.olexandr@gmail.com>
2021-07-02 17:37:15 +0200

    Culinder

--- Assets/ObjectSpawner.cs
+++ Assets/ObjectSpawner.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
 public Transform perfabToSpawn;
+public Transform perfabToSpawn2;
 public int objectCount = 50;
 public float spawnRadius = 5;
 
 void Start(){
     for(int i = 0; i < objectCount; i++){
         Vector3 spawnPoint = transform.position + Random.insideUnitSphere * spawnRadius;
+        if(i % 2 == 0)
         Instantiate(perfabToSpawn,spawnPoint,perfabToSpawn.rotation);
+        else
+        Instantiate(perfabToSpawn2,spawnPoint,perfabToSpawn.rotation);
     }
 }
 
