687d1b286ff0e788ae8ff3153dba0910da3778cc
Golem <segeda.olexandr@gmail.com>
2021-07-02 18:37:34 +0200

    Spawner

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -24,11 +24,36 @@ public class playerMovement : MonoBehaviour
             rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
             onGround = false;
         }
+
+        
+
     }
+    public Transform perfabToSpawn;
+    public Transform perfabToSpawn2;
+
+    bool isCollide = true;
+
     private void OnCollisionEnter(Collision collision)
     {
         onGround = true;
+   
+        if(collision.collider.tag == "Spawner"){
+            
+            if(isCollide){
+                Debug.Log("wer");
+                for(int i = 0; i < 200; i++){
+                    Vector3 spawnPoint = transform.position + Random.insideUnitSphere * 40;
+                    if(i % 2 == 0)
+                    Instantiate(perfabToSpawn,spawnPoint,perfabToSpawn.rotation);
+                    else
+                    Instantiate(perfabToSpawn2,spawnPoint,perfabToSpawn.rotation);
+                }
+                isCollide = false;
+            }
+        }
     }
+
+
 
 
 }
