b0eeab802c57fc43cbace86d78f69dd3573bd4fb
tommercoder <tommerstore@gmail.com>
2021-07-18 13:41:51 +0300

    [TS] joysticks and camera

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Cinemachine;
 public class playerMovement : MonoBehaviour
 {
     Rigidbody rb;
@@ -12,18 +12,60 @@ public class playerMovement : MonoBehaviour
     public int currentJump = 0;
     public Camera cam;
     public Joystick joystick;
+    public Joystick cameraJoystick;
+    public CinemachineFreeLook cinemachine;
     float xSpeed;
     float zSpeed;
     public float speedOnAir = 5;
+    public float TouchSensitivity_x = 5;
+    public float TouchSensitivity_y = 5;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //CinemachineCore.GetInputAxis = HandleAxisInputDelegate;
     }
+    //float HandleAxisInputDelegate(string axisName)
+    //{
+    //    switch (axisName)
+    //    {
 
+    //        case "Mouse X":
+
+    //            if (Input.touchCount > 0)
+    //            {
+    //                return Input.touches[0].deltaPosition.x / TouchSensitivity_x;
+    //            }
+    //            else
+    //            {
+    //                return Input.GetAxis(axisName);
+    //            }
+
+    //        case "Mouse Y":
+    //            if (Input.touchCount > 0)
+    //            {
+    //                return Input.touches[0].deltaPosition.y / TouchSensitivity_y;
+    //            }
+    //            else
+    //            {
+    //                return Input.GetAxis(axisName);
+    //            }
+
+    //        default:
+    //            Debug.LogError("Input <" + axisName + "> not recognyzed.", this);
+    //            break;
+    //    }
+
+    //    return 0f;
+    //}
     private void Update()
     {
-        xSpeed = Input.GetAxis("Horizontal");//joystick.Horizontal;
-        zSpeed = Input.GetAxis("Vertical");//joystick.Vertical;
+        xSpeed =joystick.Horizontal; //Input.GetAxis("Horizontal");
+        zSpeed = joystick.Vertical; //Input.GetAxis("Vertical");
+
+        cinemachine.m_XAxis.Value = cameraJoystick.Horizontal;
+        cinemachine.m_YAxis.Value = cameraJoystick.Vertical;
+
+
         Vector3 forward = cam.transform.forward;
         Vector3 right = cam.transform.right;
 
